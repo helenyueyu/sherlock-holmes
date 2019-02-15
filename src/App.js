4160e98313068021fd71f8ceb401672e4a0dbb57
tokyosuite <helenyueyu@gmail.com>
2019-02-15 10:52:22 -0800

    The sentiment scores can be displayed in the UI.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import array from './components/Output'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        {array.map(x => <li>{x}</li>)}
       </div>
     );
   }
